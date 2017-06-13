7ecefaaa12e82399a159665cf4d1bf75d79fd375
LAPTOP-8DJ1QH41\achak <kogeusagi12@gmail.com>
2017-06-13 21:38:01 +0900

    Postback

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ foreach ($events as $event) {
   // イベントがPostbackEventクラスのインスタンスであれば
   if ($event instanceof \LINE\LINEBot\Event\PostbackEvent) {
     // テキストを返信し次のイベントの処理へ
-    replyTextMessage($bot, $event->getReplyToken(, 'Postback受信 「' . $event->getPostbackData() . '」');
+    replyTextMessage($bot, $event->getReplyToken(), 'Postback受信 「' . $event->getPostbackData() . '」');
     continue;
   }
 
